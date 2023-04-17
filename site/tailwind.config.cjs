d6f67eb743b767bedd9c955ff037e356775ae231
greensoap <william.sjokvist@gmail.com>
2023-04-17 05:25:16 +0200

    Added layouts to tailwind config

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     './src/pages/**/*.{astro,html,js}',
     './src/components/**/*.{astro,html,js}',
+    './src/layouts/**/*.{astro,html,js}',
+
   ],
   theme: {
     // ...
